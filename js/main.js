74a62a4e8290d521fee8eee279eba32cd0b3abc7
AlenaKatt <katt.elena@yandex.ru>
2020-11-11 13:07:01 +0300

    Личный проект: больше деталей (часть 1) - поправка

--- js/main.js
+++ js/main.js
@@ -3,51 +3,47 @@
 let template = document.querySelector(`#picture`).content.querySelector(`a`);
 let keksContent = document.querySelector(`.pictures`);
 
-// let AUTHOR_NAMES = [`Алиса`, `Матвей`, `Игорь`, `Цезарь`, `Афродита`, `Клон`];
 let MESSAGE_ARRAY = [`Всё отлично!`, `В целом всё неплохо. Но не всё.`, `Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.`, `Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.`, `Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.`, `Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!`];
 
 function getRandomInRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-let getObject = function () {
-  let jsObject;
-  let newArray = [];
-  let newNumber = getRandomInRange(1, 2);
+let getArrayDataPhoto = function () {
+  let objectDataPhoto;
+  let arrayDataPhoto = [];
+  let newRandomNumber = getRandomInRange(1, 2);
 
-  for (let i = 0; i < newNumber; i++) {
+  for (let i = 0; i < newRandomNumber; i++) {
     let newComment = getRandomInRange(1, 6);
     MESSAGE_ARRAY[i] = MESSAGE_ARRAY[newComment];
   }
 
   for (let i = 0; i < 25; i++) {
-    let photo = `photos/` + [i + 1] + `.jpg`;
+    let photoObject = `photos/` + [i + 1] + `.jpg`;
     let lineDescription = ``;
-    jsObject = {
-      url: photo,
+    objectDataPhoto = {
+      url: photoObject,
       description: lineDescription,
       likes: getRandomInRange(15, 200),
       comments: MESSAGE_ARRAY[i]
     };
-    newArray.push(jsObject);
+    arrayDataPhoto.push(objectDataPhoto);
   }
-  return newArray;
+  return arrayDataPhoto;
 };
 
-getObject();
-
-let contentArray = getObject();
+let contentArray = getArrayDataPhoto();
 
 for (let i = 0; i < 25; i++) {
-  let element = template.cloneNode(true);
+  let elementContent = template.cloneNode(true);
 
-  // let pictureImg = element.querySelector(`.picture__img`).src;
-  let pictureLikes = element.querySelector(`.picture__likes`);
-  let pictureComments = element.querySelector(`.picture__comments`);
+  let pictureLikes = elementContent.querySelector(`.picture__likes`);
+  let pictureComments = elementContent.querySelector(`.picture__comments`);
 
-  element.querySelector(`.picture__img`).src = contentArray[i].url;
+  elementContent.querySelector(`.picture__img`).src = contentArray[i].url;
 
   pictureLikes.textContent = contentArray[i].likes;
   pictureComments.textContent = contentArray[i].comments;
-  keksContent.appendChild(element);
+  keksContent.appendChild(elementContent);
 }
